ad22d97e8961ebb0833b484ab0765bc0e62b6e8f
w479912090 <479912090@qq.com>
2019-03-25 20:59:46 +0800

    Add files via upload

---
+++ 使用list和tuple.py
@@ -0,0 +1,10 @@
+classmates = ['Michael', 'Bob', 'Tracy']
+#用-1做索引，直接获取最后一个元素
+print(classmates[-1])
+#以此类推，可以获取倒数第2个、倒数第3个
+print(classmates[-2])
+print(classmates[-3])
+
+#使用append在list后面添加元素
+classmates.append('Adam')
+print(classmates[-1]);
